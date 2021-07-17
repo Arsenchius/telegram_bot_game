ad40672cf92bf8d54244dd75def41089e4d9feae
Destrokhen-main <armagiddon19@gmail.com>
2021-07-17 19:43:46 +0300

    Done Parser

--- parsers.py
+++ parsers.py
@@ -1,12 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
-
+from random import randint
+ 
 avito_url = 'https://www.avito.ru/?q='
-
 
 # коды ответов
 # 500 - не смог получить цифры
 # 400 - Ошибка в запросе
+
+# Лист со всеми словами
+class object:
+    def __init__(self,name,count):
+        self.name = name
+        self.count = count
+
+def take_words(num):
+    file = open("word_rus.txt")
+    pack = []
+    for i in file:
+        pack.append(i.replace("\n",""))
+
+    total_pack = []
+
+    for i in range(1,num):
+        num = randint(0,len(pack))
+        total_pack.append(pack[num])
+    
+    return total_pack
 
 
 def main(question):
@@ -15,8 +35,21 @@ def main(question):
         soup = BeautifulSoup(response.text,"lxml")
         try:
             temp = soup.find('span', class_='page-title-count-1oJOc').text.strip()
+            return temp
         except:
             return 500
-        return temp
     else:
         return 400 
+
+
+def complite_pack(num):
+    pack = take_words(num)
+
+    total = []
+
+    for i in pack:
+        count = main(i)
+        total.append(object(i,count))
+    
+    return total
+
