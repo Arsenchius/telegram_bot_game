3703be1e469ee64f4040815084b9c40866b70cea
Destrokhen-main <armagiddon19@gmail.com>
2021-07-17 19:03:55 +0300

    Изменил parser

--- parsers.py
+++ parsers.py
@@ -1,13 +1,22 @@
-from warnings import resetwarnings
 from bs4 import BeautifulSoup
 import requests
 
 avito_url = 'https://www.avito.ru/?q='
 
+
+# коды ответов
+# 500 - не смог получить цифры
+# 400 - Ошибка в запросе
+
+
 def main(question):
     response = requests.get(avito_url+question)
     if response.status_code == 200:
-        print("Успешно")
         soup = BeautifulSoup(response.text,"lxml")
-        temp = soup.find('span', class_='page-title-count-1oJOc').text.strip()
+        try:
+            temp = soup.find('span', class_='page-title-count-1oJOc').text.strip()
+        except:
+            return 500
         return temp
+    else:
+        return 400 
